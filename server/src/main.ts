2df3bd5f685292d554a772bbd66fa2653c87c0fb
unknown <u9067156@bsmch.net>
2026-06-12 16:24:57 +0300

    feat(client): init front

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,7 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '' });
+  app.enableCors({ origin: 'http://localhost:5173' });
   app.use(helmet());
   const config = new DocumentBuilder()
     .setTitle('Locations')
