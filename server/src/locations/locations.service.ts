6e56ca3353145cb63b1c76a79e887e4ee8f68c7b
unknown <u9067156@bsmch.net>
2026-06-12 13:04:59 +0300

    feat(server): add try catch (dont know how to use filters well enough)

--- server/src/locations/locations.service.ts
+++ server/src/locations/locations.service.ts
@@ -2,6 +2,8 @@ import {
   Injectable,
   NotFoundException,
   BadRequestException,
+  ServiceUnavailableException,
+  HttpException,
 } from '@nestjs/common';
 import { CreateLocationDto } from './dto/create-location.dto';
 import { UpdateLocationDto } from './dto/update-location.dto';
@@ -17,37 +19,73 @@ export class LocationsService {
   ) {}
 
   async create(createLocationDto: CreateLocationDto): Promise<Location> {
-    const createdLocation = new this.locationModel(createLocationDto);
-    return await createdLocation.save();
+    try {
+      const createdLocation = new this.locationModel(createLocationDto);
+      return await createdLocation.save();
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new ServiceUnavailableException(
+        'Oops! something went wrong. Please try again later',
+      );
+    }
   }
 
   async findAll(): Promise<Location[]> {
-    return this.locationModel.find().exec();
+    try {
+      return this.locationModel.find().exec();
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new ServiceUnavailableException(
+        'Oops! something went wrong. Please try again later',
+      );
+    }
   }
 
   async update(id: string, updateLocationDto: UpdateLocationDto) {
     if (!isValidObjectId(id)) {
       throw new BadRequestException('The provided id is invalid');
     }
-    const updatedLocation = await this.locationModel
-      .findByIdAndUpdate(id, updateLocationDto, { new: true })
-      .exec();
-    if (!updatedLocation) {
-      throw new NotFoundException(`There is no location with id ${id}`);
+    try {
+      const updatedLocation = await this.locationModel
+        .findByIdAndUpdate(id, updateLocationDto, { new: true })
+        .exec();
+      if (!updatedLocation) {
+        throw new NotFoundException(`There is no location with id ${id}`);
+      }
+      return updatedLocation;
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new ServiceUnavailableException(
+        'Oops! something went wrong. Please try again later',
+      );
     }
-    return updatedLocation;
   }
 
   async remove(id: string) {
     if (!isValidObjectId(id)) {
       throw new BadRequestException('The provided id is invalid');
     }
-    const deletedLocation = await this.locationModel
-      .findByIdAndDelete(id, { new: true })
-      .exec();
-    if (!deletedLocation) {
-      throw new NotFoundException(`There is no location with id ${id}`);
+    try {
+      const deletedLocation = await this.locationModel
+        .findByIdAndDelete(id, { new: true })
+        .exec();
+      if (!deletedLocation) {
+        throw new NotFoundException(`There is no location with id ${id}`);
+      }
+      return deletedLocation;
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new ServiceUnavailableException(
+        'Oops! something went wrong. Please try again later',
+      );
     }
-    return deletedLocation;
   }
 }
