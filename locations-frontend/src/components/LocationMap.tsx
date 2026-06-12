aba2785308fcd23bd7d30904e1ddf9bb19612993
unknown <u9067156@bsmch.net>
2026-06-12 18:10:55 +0300

    feat(client): add points on map!

--- locations-frontend/src/components/LocationMap.tsx
+++ locations-frontend/src/components/LocationMap.tsx
@@ -1,4 +1,4 @@
-import { Button, Container, Grid } from '@mui/material'
+import { Container } from '@mui/material'
 import { useEffect, useRef } from 'react'
 import Map from 'ol/Map.js'
 import View from 'ol/View.js'
@@ -11,7 +11,6 @@ import Point from 'ol/geom/Point.js'
 import VectorLayer from 'ol/layer/Vector.js'
 import { fromLonLat } from 'ol/proj.js'
 import VectorSource from 'ol/source/Vector.js'
-import Icon from 'ol/style/Icon.js'
 import Style from 'ol/style/Style.js'
 import Circle from 'ol/style/Circle.js'
 import Fill from 'ol/style/Fill'
@@ -51,7 +50,34 @@ export const LocationMap = ({ locations }: LocationMapProps) => {
       map.setTarget(undefined)
     }
   }, [])
+  useEffect(() => {
+    if (!vectorSourceRef.current) return
 
+    // Clear any existing markers
+    vectorSourceRef.current.clear()
+
+    if (!locations || locations.length === 0) return
+
+    const basicStyle = new Style({
+      image: new Circle({
+        fill: new Fill({ color: 'rgba(255, 0, 0, 0.7)' }),
+        radius: 8,
+      }),
+    })
+
+    const points = locations.map((location) => {
+      const { lon, lat } = location.coordinates
+
+      const point = new Feature({
+        geometry: new Point(fromLonLat([lon, lat])),
+      })
+
+      point.setStyle(basicStyle)
+      return point
+    })
+
+    vectorSourceRef.current.addFeatures(points)
+  }, [locations])
   return (
     <Container>
       <div id="map" style={{ width: '100%', height: '400px' }} />
