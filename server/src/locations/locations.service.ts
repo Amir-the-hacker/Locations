7618913e36b86550ba46ce4fecd94e92dc3a2656
unknown <u9067156@bsmch.net>
2026-06-12 12:58:33 +0300

    fix(server): exception status wasnt right

--- server/src/locations/locations.service.ts
+++ server/src/locations/locations.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
 import { CreateLocationDto } from './dto/create-location.dto';
 import { UpdateLocationDto } from './dto/update-location.dto';
 import { InjectModel } from '@nestjs/mongoose';
@@ -22,16 +26,21 @@ export class LocationsService {
   }
 
   async update(id: string, updateLocationDto: UpdateLocationDto) {
+    if (!isValidObjectId(id)) {
+      throw new BadRequestException('The provided id is invalid');
+    }
     const updatedLocation = await this.locationModel
       .findByIdAndUpdate(id, updateLocationDto, { new: true })
       .exec();
-
+    if (!updatedLocation) {
+      throw new NotFoundException(`There is no location with id ${id}`);
+    }
     return updatedLocation;
   }
 
   async remove(id: string) {
     if (!isValidObjectId(id)) {
-      throw new NotFoundException(`The provided id is invalid`);
+      throw new BadRequestException('The provided id is invalid');
     }
     const deletedLocation = await this.locationModel
       .findByIdAndDelete(id, { new: true })
