cb453b76df562d6c2eadb45b654973040c0293dc
unknown <u9067156@bsmch.net>
2026-06-12 16:18:32 +0300

    feat(server): basic securety

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { cleanupOpenApiDoc } from 'nestjs-zod';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({ origin: '' });
+  app.use(helmet());
   const config = new DocumentBuilder()
     .setTitle('Locations')
     .setDescription('The locations API description')
